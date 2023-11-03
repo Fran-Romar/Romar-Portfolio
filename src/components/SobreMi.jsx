4db329a0a2abc88db8c19e4abb8a7051c5e16c21
Fran-Romar <franromar2002@mail.com>
2023-11-03 13:57:19 +0100

    Update

--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -6,7 +6,7 @@ export default function SobreMi() {
       <h3>
         <em className="em-title">Sobre</em> mí
       </h3>
-      <p>
+      {/* <p>
         <em className="em-font">
           ¡Hola! Soy <em className="em-title">Francisco Javier</em>
         </em>
@@ -20,6 +20,21 @@ export default function SobreMi() {
         para enfrentar nuevos desafíos en este campo{" "}
         <em className="imp-content">dinámico</em> y{" "}
         <em className="imp-content">emocionante</em>.
+      </p> */}
+      <p>
+        ¡Hola! Soy <em className="em-title">Francisco Javier</em>, un estudiante
+        de <em className="em-title">21 años</em> de{" "}
+        <em className="imp-content">Ingeniería Informática</em> y un apasionado
+        desarrollador web. Mi pasión por crear experiencias digitales asombrosas
+        me ha llevado a explorar el emocionante mundo del{" "}
+        <em className="imp-content">desarrollo web</em>. Con un enfoque en la
+        <em className="imp-content">innovación</em> y la{" "}
+        <em className="imp-content">creatividad</em>, disfruto transformando
+        ideas en sitios web dinámicos y atractivos.
+      </p>
+      <p>
+        Estoy emocionado por las oportunidades que el desarrollo web ofrece y
+        estoy listo para enfrentar nuevos desafíos en este apasionante campo.
       </p>
     </section>
   );
