195b742f8b34cf9387052620344a93ddd0f23e5d
Fran-Romar <franromar2002@mail.com>
2023-11-05 21:22:51 +0100

    Contacto form

--- src/components/Contacto.jsx
+++ src/components/Contacto.jsx
@@ -1,5 +1,29 @@
 import React from "react";
 
 export default function Contacto() {
-  return <section id="Contacto">Contacto</section>;
+  return (
+    <section id="Contacto">
+      <h3>
+        <em className="em-title">Contacto</em>
+      </h3>
+      <form>
+        <div>
+          <label htmlFor="name">Nombre</label>
+          <br />
+          <input type="text" name="name" id="name" />
+        </div>
+        <div>
+          <label htmlFor="email">Correo</label>
+          <br />
+          <input type="email" name="email" id="email" />
+        </div>
+        <div>
+          <label htmlFor="asunto">Asunto</label>
+          <br />
+          <textarea name="asunto" id="asunto" cols="30" rows="7"></textarea>
+        </div>
+        <button type="submit">Enviar</button>
+      </form>
+    </section>
+  );
 }
--- src/components/SobreMi.jsx
+++ src/components/SobreMi.jsx
@@ -6,21 +6,6 @@ export default function SobreMi() {
       <h3>
         <em className="em-title">Sobre</em> mí
       </h3>
-      {/* <p>
-        <em className="em-font">
-          ¡Hola! Soy <em className="em-title">Francisco Javier</em>
-        </em>
-        , un estudiante de <em className="em-title">21 años</em> de{" "}
-        <em className="imp-content">Ingeniería Informática</em> y un apasionado
-        desarrollador web.
-      </p>
-      <p>
-        Estoy emocionado por las oportunidades que ofrece el{" "}
-        <em className="imp-content">desarrollo web</em> y siempre estoy listo
-        para enfrentar nuevos desafíos en este campo{" "}
-        <em className="imp-content">dinámico</em> y{" "}
-        <em className="imp-content">emocionante</em>.
-      </p> */}
       <p>
         ¡Hola! Soy <em className="em-title">Francisco Javier</em>, un estudiante
         de <em className="em-title">21 años</em> de{" "}
